a2a29c6e3d8dd6093d4eacc012e8bc1556264fa4
matt laptop <mattmiller899@email.arizona.edu>
2019-10-09 14:22:15 -0700

    pushing r exercise 3 and the notes example code

---
+++ project/notes_r_exercise.R
@@ -0,0 +1,38 @@
+(state_table <- data.frame(key=c("CA", "NY", "WA", "ON", "QU"),
+                           name=c("California", "new York", "Washington", "Ontario", "Quebec"),
+                           country=c("USA", "USA", "USA", "Canada", "Canada")))
+(month_table <- data.frame(key=1:12,
+                          desc=c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+                          quarter=c("Q1","Q1","Q1","Q2","Q2","Q2","Q3","Q3","Q3","Q4","Q4","Q4")))
+(prod_table <- data.frame(key=c("Printer", "Tablet", "Laptop"),
+                          price=c(225, 570, 1120)))
+
+gen_sales <- function(no_of_recs) {
+  loc <- sample(state_table$key, no_of_recs, 
+                replace=T, prob=c(2,2,1,1,1))
+  time_month <- sample(month_table$key, no_of_recs, replace=T)
+  time_year <- sample(c(2012, 2013), no_of_recs, replace=T)
+  prod <- sample(prod_table$key, no_of_recs, replace=T, prob=c(1, 3, 2))
+  unit <- sample(c(1,2), no_of_recs, replace=T, prob=c(10, 3)) #if the chance for '1' to be sampled is 10, then the chance for '2' to be sampled is 3.
+  amount <- unit*prod_table[prod,]$price
+  sales <- data.frame(month=time_month,
+                      year=time_year,
+                      loc=loc,
+                      prod=prod,
+                      unit=unit,
+                      amount=amount)
+        # Sort the records by time order
+  sales <- sales[order(sales$year, sales$month),]
+  row.names(sales) <- NULL
+  return(sales)
+}
+
+(sales_fact <- gen_sales(500))
+View(sales_fact)
+head(sales_fact)
+
+(revenue_cube <- tapply(sales_fact$amount, 
+                        sales_fact[,c("prod", "month", "year", "loc")],
+                        FUN=function(x){return(sum(x))}))
+
+
